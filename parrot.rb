b74153c3052ea49d648ee9acb4d9658d170687bc
andy reyes <11710reyes@gmail.com>
2018-12-19 00:37:00 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot(phrase = "Squawk!")
+irb(main):002:1> puts phrase
+irb(main):003:1> return phrase
+irb(main):004:1> end
